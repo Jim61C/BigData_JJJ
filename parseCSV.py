eceef1fc32c2c7bc489c9ccf967daf135d424444
JimXing <xyf.jim@gmail.com>
2015-04-17 20:59:15 -0400

    Finish parsing, data file and label file stored

--- parseCSV.py
+++ parseCSV.py
@@ -31,7 +31,12 @@ def main():
 	# 1 for No readimission
 	# 2 for <30 days
 	# 3 for >30 days
-	label_readmission = []
+	label_readmission = np.zeros(N_SAMPLES,dtype = int)
+	readmission_Dict = {
+	'NO':1,
+	'<30':2,
+	'>30':3
+	}
 
 	# HBA1C label: 
 	# 4 classes:
@@ -39,23 +44,29 @@ def main():
 	# 2 for >7
 	# 3 for Norm (<7)
 	# 4 for None
-	label_HBA1C = []
+	label_HBA1C = np.zeros(N_SAMPLES,dtype = int)
+	HBA1C_Dict = {
+	'>8':1,
+	'>7':2,
+	'Norm':3,
+	'None':4
+	}
 	
 	# Primary Diagonosis Label
 	# 9 classes:
-	#A disease of the circulatory system
-	#Diabetes
-	#A disease of the respiratory system
-	#Diseases of the digestive system
-	#Injury and poisoning
-	#Diseases of the musculoskeletal system and connective tissue
-	#Diseases of the genitourinary system
-	#Neoplasms
-	#Other
-	label_diag1 = []
+	#1 for A disease of the circulatory system
+	#2 for Diabetes
+	#3 for A disease of the respiratory system
+	#4 for Diseases of the digestive system
+	#5 for Injury and poisoning
+	#6 for Diseases of the musculoskeletal system and connective tissue
+	#7 for Diseases of the genitourinary system
+	#8 for Neoplasms
+	#9 for Other
+	label_diag1 = np.zeros(N_SAMPLES,dtype = int)
 
 	# Medication change label: binary, 0 for NO, 1 for Yes
-	label_medication_change = []
+	label_medication_change = np.zeros(N_SAMPLES,dtype = int)
 
 
 	# Numerical Data Array
@@ -70,6 +81,9 @@ def main():
 	'No':0
 	}
 
+	missingcount = 0
+	E_count = 0
+	V_count = 0
 	with open('diabetic_data.csv') as csvfile:
 		reader = csv.reader(csvfile, delimiter = ',')
 		i = 0
@@ -77,22 +91,83 @@ def main():
 		iterrows = iter(reader)
 		next(iterrows)
 		for row in iterrows:
-			# parse Age	
+			#parse Age, age is in column 4
 			age = int(row[4][1:row[4].find('-')])
 			data_numerical[i][0] = age/10 # age class is just age/10, TODO: try with just 0,10,20,30...etc year old categories
-			# parse Time in the hospital
+			#parse Time in the hospital, which is column 9
 			data_numerical[i][1] = int (row[9])
-			# parse the rest of the numerical data
+			#parse the rest of the numerical data, which is in column 12:18
 			data_numerical[i][2:] = [int(num) for num in row [12:18]]
 
-			#parse bag of drugs
+			#parse bag of drugs, which is in column 24:47
 			drug_dosage = row[24:47]
 			for j in range (0, len(drug_dosage)):
 				data_bagOfDrugs[i][j] = bagOfDrugs_Dict[drug_dosage[j]]
+			
+			#parse the medication change label, which is the 3rd last column
+			if(row[-3] == 'No'):
+				label_medication_change[i]=0
+			else:
+				label_medication_change[i]=1
+
+			#parse the readimission label, which is last column
+			label_readmission[i] = readmission_Dict[row[-1]]
+			#parse the HBA1C test label, which is column 23
+			label_HBA1C[i] = HBA1C_Dict[row[23]]
+			#parse the Primary Diagonosis, which is column 18
+			print "The Diag1 Raw:", row[18]
+			if(row[18].find('?') != -1):
+				label_diag1[i] = 9
+				missingcount +=1
+			elif(row[18].find('V') !=-1):
+				label_diag1[i] = 9
+				V_count += 1
+			elif(row[18].find('E')!=-1):
+				label_diag1[i] = 9
+				E_count += 1
+			else:
+
+				diag1 = int(float(row[18]))
+
+				print diag1
+				if(( diag1>= 390 and diag1<=459) or diag1 == 785):
+					label_diag1[i] = 1
+				elif(diag1 == 250):
+					label_diag1[i] = 2
+				elif((diag1 >= 468 and diag1 <=519) or diag1 == 786):
+					label_diag1[i] = 3
+				elif((diag1 >= 520 and diag1 <=579) or diag1 == 787):
+					label_diag1[i] = 4
+				elif(diag1 >= 800 and diag1 <=999):
+					label_diag1[i] = 5
+				elif(diag1 >= 710 and diag1 <=739):
+					label_diag1[i] = 6
+				elif((diag1 >= 580 and diag1 <=629) or diag1 == 788):
+					label_diag1[i] = 7
+				elif(diag1 >= 140 and diag1 <=239):
+					label_diag1[i] = 8
+				else:
+					label_diag1[i] = 9
+
 			i+= 1
 
-	print data_numerical[-1:,] # see the last patient for verification
-	print data_bagOfDrugs
+	print "data_numerical:\n",data_numerical[-1:,] # see the last patient for verification
+	print "data_bagOfDrugs:\n",data_bagOfDrugs
+	print "label_readmission:\n",label_readmission
+	print "label_medication_change:\n", label_medication_change
+	print "label_HBA1C:\n", label_HBA1C
+	print "label_diag1:\n", label_diag1
+	
+	print "\nE_count:", E_count
+	print "V_count:", V_count
+	print "missingcount:", missingcount
+
+	saveArray(data_numerical, "data_numerical")
+	saveSparse(csc_matrix(data_bagOfDrugs), "data_bagOfDrugs_sparse")
+	saveArray(label_readmission, "label_readmission")
+	saveArray(label_diag1,"label_diag1")
+	saveArray(label_HBA1C, "label_HBA1C")
+	saveArray(label_medication_change, "label_medication_change")
 
 	#print np.asarray(label_readmission)
 	return
